6771bc34e3bd998d2cef7588abdf7470d5141c57
Mario <mrivera_dominguez@hotmail.com>
2014-08-02 14:14:43 -0500

    Ejemplos del método System.out.println

--- AirportSimulator/src/com/mariouz/aeropuerto/Main.java
+++ AirportSimulator/src/com/mariouz/aeropuerto/Main.java
@@ -6,6 +6,17 @@ public class Main {
 		System.out.println("El método para ejecutar programas es el método main: ");
 		System.out.println("public static void main (String[] args){}");
 		System.out.println("");
-		
+		System.out.println("El método para escribir en la consola :");
+		System.out.println("System.out.println(\"Cualquier cadena/numero/objeto\")");
+		System.out.println("El clasico Hola Mundo");
+		System.out.println("Condiciones Booleanas:");
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println("Números: ");
+		System.out.println(12345);
+		System.out.println("y Objetos");
+		System.out.println(new Avion());
+		System.out.println("o la combinacion de varios elementos");
+		System.out.println(1+")Avion:  "+new Avion());
 	}
 }
