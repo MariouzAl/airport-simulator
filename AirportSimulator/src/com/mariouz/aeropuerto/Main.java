ea114755cc9684975720f3b6a8f8fa461acb34e3
Mario <mrivera_dominguez@hotmail.com>
2014-08-02 14:13:59 -0500

    Clase main con explicación

--- AirportSimulator/src/com/mariouz/aeropuerto/Main.java
+++ AirportSimulator/src/com/mariouz/aeropuerto/Main.java
@@ -2,6 +2,10 @@ package com.mariouz.aeropuerto;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("==ESTA ES LA CLASE PRINCIPAL DE NUESTRO PROGRAMA: Main==");
+		System.out.println("El método para ejecutar programas es el método main: ");
+		System.out.println("public static void main (String[] args){}");
+		System.out.println("");
 		
 	}
 }
