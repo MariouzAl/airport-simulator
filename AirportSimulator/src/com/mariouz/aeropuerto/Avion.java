b8d5370bacdb09731432352778beb54b966461c3
Mario <mrivera_dominguez@hotmail.com>
2014-08-03 00:51:25 -0500

    Metodos set y get agregados a clase Avion

--- AirportSimulator/src/com/mariouz/aeropuerto/Avion.java
+++ AirportSimulator/src/com/mariouz/aeropuerto/Avion.java
@@ -5,4 +5,28 @@ public class Avion {
 	private String aerolinea;
 	private String motor;
 	private int combustible;
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getAerolinea() {
+		return aerolinea;
+	}
+	public void setAerolinea(String aerolinea) {
+		this.aerolinea = aerolinea;
+	}
+	public int getCombustible() {
+		return combustible;
+	}
+	public void setCombustible(int combustible) {
+		this.combustible = combustible;
+	}
+	public String getMotor() {
+		return motor;
+	}
+	public void setMotor(String motor) {
+		this.motor = motor;
+	}
 }
