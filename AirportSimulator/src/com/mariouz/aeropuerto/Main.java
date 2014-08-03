b6b0b36f947daced68818391f792e5c1a48b24c8
Mario <mrivera_dominguez@hotmail.com>
2014-08-03 02:52:24 -0500

    Creacion de variable/objeto Avion e invocación de sus métodos

--- AirportSimulator/src/com/mariouz/aeropuerto/Main.java
+++ AirportSimulator/src/com/mariouz/aeropuerto/Main.java
@@ -2,21 +2,12 @@ package com.mariouz.aeropuerto;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("==ESTA ES LA CLASE PRINCIPAL DE NUESTRO PROGRAMA: Main==");
-		System.out.println("El método para ejecutar programas es el método main: ");
-		System.out.println("public static void main (String[] args){}");
-		System.out.println("");
-		System.out.println("El método para escribir en la consola :");
-		System.out.println("System.out.println(\"Cualquier cadena/numero/objeto\")");
-		System.out.println("El clasico Hola Mundo");
-		System.out.println("Condiciones Booleanas:");
-		System.out.println(true);
-		System.out.println(false);
-		System.out.println("Números: ");
-		System.out.println(12345);
-		System.out.println("y Objetos");
-		System.out.println(new Avion());
-		System.out.println("o la combinacion de varios elementos");
-		System.out.println(1+")Avion:  "+new Avion());
+		Avion avion =  new Avion();
+		avion.setAerolinea("Interjet");
+		avion.setCombustible(100);
+		avion.setMotor("Rolls-Royce");
+		avion.setNumero(1234);
+		
+		System.out.println(avion.getNumero()+" "+avion.getAerolinea());
 	}
 }
